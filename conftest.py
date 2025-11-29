43fe5fd38dd29cec67a3f52da8aa89096fe99bdc
svyas02 <svyas02@uoguelph.ca>
2025-11-28 23:10:36 -0500

    sdfdfs

--- conftest.py
+++ conftest.py
@@ -2,13 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-import pytest
-
-@pytest.fixture
-def driver():
-    service = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")  # if you want headless
-    driver = webdriver.Chrome(service=service, options=options)
-    yield driver
-    driver.quit()
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
