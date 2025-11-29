01329b473df02611149a39e43d8dbf4e3e76c58a
svyas02 <svyas02@uoguelph.ca>
2025-11-28 22:06:11 -0500

    fsdfdfsfd

--- conftest.py
+++ conftest.py
@@ -1,9 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 @pytest.fixture
 def driver():
-    options = webdriver.ChromeOptions()
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
     driver = webdriver.Remote(
         command_executor="http://localhost:4444/wd/hub",
-        options=options
+        options=chrome_options
     )
+
     yield driver
     driver.quit()
